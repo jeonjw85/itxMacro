f7b78bd564541d1178c44620998223a0548269f8
Jeon Jungwan <jjw@jjw.kr>
2022-12-11 04:00:29 +0900

    결제추가 및 속도조정

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import json
 options = webdriver.ChromeOptions()
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 options.add_argument('user-agent=' + user_agent)
-options.add_argument("--headless")
-options.add_argument("no-sandbox")
+# options.add_argument("--headless")
+# options.add_argument("no-sandbox")
 options.add_argument('--mute-audio')
 
 try:
@@ -32,18 +32,15 @@ try:
     driver.switch_to.window(driver.window_handles[0])
     time.sleep(0.7)
 
-    xpath1 = "//input[@id='txtGoStart']"
-    driver.find_element(By.XPATH, xpath1).clear()
-    driver.find_element(By.XPATH, xpath1).send_keys("평내호평")
+    driver.find_element(By.XPATH, "//input[@id='txtGoStart']").clear()
+    driver.find_element(By.XPATH, "//input[@id='txtGoStart']").send_keys("평내호평")
     time.sleep(0.7)
 
-    xpath2 = "//input[@id='txtGoEnd']"
-    driver.find_element(By.XPATH, xpath2).clear()
-    driver.find_element(By.XPATH, xpath2).send_keys("용산")
+    driver.find_element(By.XPATH, "//input[@id='txtGoEnd']").clear()
+    driver.find_element(By.XPATH, "//input[@id='txtGoEnd']").send_keys("용산")
     time.sleep(0.7)
 
-    xpath3 = "//img[@alt='달력']"
-    driver.find_element(By.XPATH, xpath3).click()
+    driver.find_element(By.XPATH, "//img[@alt='달력']").click()
     time.sleep(0.7)
 
     driver.switch_to.window(driver.window_handles[1])
@@ -51,18 +48,16 @@ try:
 
     if dt.strftime("%m") == "12": nxtmonth = "01"
     else: nxtmonth = dt.strftime("%m")+1
-    xpath4 = "//span[@id='d2023%s%s']"
     driver.find_element(By.XPATH, xpath4 % (nxtmonth, dt.strftime("%d"))).click()
     time.sleep(0.7)
 
     driver.switch_to.window(driver.window_handles[0])
     time.sleep(0.7)
 
-    Select(driver.find_element(By.XPATH, '//select[@id="time"]')).select_by_value('11')
+    Select(driver.find_element(By.XPATH, '//select[@id="time"]')).select_by_value('7')
     time.sleep(0.7)
 
-    xpath5 = "//img[@alt='승차권예매']"
-    driver.find_element(By.XPATH, xpath5).click()
+    driver.find_element(By.XPATH, "//img[@alt='승차권예매']").click()
     time.sleep(0.7)
 
     print("%s:%s:%s - 예매설정완료" % (dt.hour, dt.minute, dt.second))
@@ -75,34 +70,83 @@ try:
     time.sleep(0.7)
 
     try:
-        xpath7 = "//img[@name='btnRsv1_0']"
-        driver.find_element(By.XPATH, xpath7).click()
+        driver.find_element(By.XPATH, "//img[@name='btnRsv1_0']").click()
         time.sleep(5)
     except Exception as error:
         print("[error] 매진됐거나 오류발생")
 
-    #data['korail']['id']
-
-    xpath8 = "//input[@id='txtMember']"
-    xpath9 = "//input[@id='txtPwd']"
-    driver.find_element(By.XPATH, xpath8).send_keys(data['korail']['id'])
-    time.sleep(0.2)
-    driver.find_element(By.XPATH, xpath9).send_keys(data['korail']['pw'])
+    driver.find_element(By.XPATH, "//input[@id='txtMember']").send_keys(data['korail']['id'])
+    time.sleep(0.3)
+    driver.find_element(By.XPATH, "//input[@id='txtPwd']").send_keys(data['korail']['pw'])
     time.sleep(0.7)
 
     print("%s:%s:%s - 로그인성공" % (dt.hour, dt.minute, dt.second))
 
-    xpath10 = "//img[@alt='확인']"
-    driver.find_element(By.XPATH, xpath10).click()
-    time.sleep(1)
+    driver.find_element(By.XPATH, "//img[@alt='확인']").click()
+    time.sleep(0.7)
 
     driver.switch_to.alert
     Alert(driver).accept()
-    time.sleep(1)
+    time.sleep(0.7)
 
-    driver.quit()
+    # driver.quit()
 
 except Exception as error:
     print(error)
 else:
-    print("%s:%s:%s - 예매성공!\n예약일자:2023%s%s\n예약시간:%s" % (dt.hour, dt.minute, dt.second, nxtmonth, dt.strftime("%d"), "11"))
+    print("%s:%s:%s - 예매성공!\n예약일자:2023%s%s\n예약시간:%s" % (dt.hour, dt.minute, dt.second, nxtmonth, dt.strftime("%d"), "07"))
+
+try:
+    
+    Alert(driver).accept()
+    time.sleep(0.7)
+
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(0.7)
+
+    driver.find_element(By.XPATH, "//a[@title='결제하기']").click()
+    time.sleep(0.7)
+
+    driver.find_element(By.XPATH, "//input[@id='chk_smpl_sb']").click()
+    time.sleep(0.7)
+
+    driver.find_element(By.XPATH, "//a[@id='fnIssuing']").click()
+    time.sleep(0.7)
+
+    driver.switch_to.window(driver.window_handles[1])
+    time.sleep(0.7)
+    
+    driver.find_element(By.XPATH, "//p[@class='bank_bt_pay']").click()
+    time.sleep(0.7)
+
+    driver.find_element(By.XPATH, "//input[@name='inputPwd1']").send_keys(data['autopay']['1'])
+    time.sleep(0.3)
+    driver.find_element(By.XPATH, "//input[@name='inputPwd2']").send_keys(data['autopay']['2'])
+    time.sleep(0.3)
+    driver.find_element(By.XPATH, "//input[@name='inputPwd3']").send_keys(data['autopay']['3'])
+    time.sleep(0.3)
+    driver.find_element(By.XPATH, "//input[@name='inputPwd4']").send_keys(data['autopay']['4'])
+    time.sleep(0.3)
+    driver.find_element(By.XPATH, "//input[@name='inputPwd5']").send_keys(data['autopay']['5'])
+    time.sleep(0.3)
+    driver.find_element(By.XPATH, "//input[@name='inputPwd6']").send_keys(data['autopay']['6'])
+    time.sleep(0.3)
+    driver.find_element(By.XPATH, "//p[@class='sp_bottom_bn']").click()
+    time.sleep(10)
+    
+    driver.switch_to(driver.window_handles[0])
+    time.sleep(0.7)
+
+    driver.find_element(By.XPATH, "//a[@id='btn_next2']").click()
+    time.sleep(0.3)
+
+    driver.switch_to.alert
+    Alert(driver).accept()
+    time.sleep(0.7)
+
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(10)
+except Exception as error:
+    print(error)
+else:
+    print("%s:%s:%s - 결제성공!\n예약일자:2023%s%s\n예약시간:%s" % (dt.hour, dt.minute, dt.second, nxtmonth, dt.strftime("%d"), "07"))
