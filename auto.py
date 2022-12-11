958e743a35fd85625122c42c813b098cc2780bad
Jeon Jungwan <jjw@jjw.kr>
2022-12-11 21:41:18 +0900

    정기자동예매 추가 및 버그수정

---
+++ auto.py
@@ -0,0 +1,28 @@
+import app, requests, time, schedule
+from bs4 import BeautifulSoup
+from datetime import datetime
+now = datetime.now()
+print(now.hour)
+
+def auto():
+    url = "https://dotwotnm.jjw.workers.dev"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        html = response.text
+        result = BeautifulSoup(html, "html.parser")
+
+        if result == "x":
+            print("토,일에는 예매하지않음")
+        else:
+            print("자동실행 시작 - ")
+            app.itx()
+    else:
+        print("ERROR - 접속실패")
+
+schedule.every().day.at("07:00").do(auto)
+schedule.every().day.at("07:03").do(auto)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
